7816f026acce33ea758ecf6d30da21b89f5dfc25
Kerzzi <kerzzi@outlook.com>
2018-03-25 15:43:57 +0800

    修改部分资讯页面显示效果

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,50 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @user = @article.user
+    @userarticles = @article.user.articles.recent.paginate(:page => params[:page], :per_page => 5)
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+
+    if @article.save
+      redirect_to articles_path
+      flash[:notice] = "文章已提交，待管理员审核后可发布"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path, notice: "文章更新成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, alert: "文章已删除"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content,:summary,:image, :user_id, :author, :source, :article_category_id, :status)
+  end
+
 end
