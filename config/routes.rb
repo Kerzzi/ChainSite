e614ca66eb09a6f93a7eb7261b518ab55ee3f4f8
Kerzzi <kerzzi@outlook.com>
2018-03-25 14:32:01 +0800

    删除welcome controller，完成前台的project功能

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
-  root 'welcome#index'
-  
+  root 'sites#index'
+
   resources :sites
   resources :site_nodes
   resources :article_categories
   resources :articles
   resources :projects
-  
+
   namespace :admin do
     resources :site_nodes
     resources :sites
