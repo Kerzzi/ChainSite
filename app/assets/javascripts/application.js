2381e577deb9325c4d7222e876832587288d5d2d
Kerzzi <kerzzi@outlook.com>
2018-05-30 20:33:09 +0800

    给 糖果空投添加二级导航

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,17 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+//navbar自动下拉
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
