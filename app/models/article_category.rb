25877ac7ff94c2aff9e733bfea35bafa6341a32d
Kerzzi <kerzzi@outlook.com>
2018-05-30 22:47:14 +0800

    暂不将原article_category修改为article_tag

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,5 +1,6 @@
 class ArticleCategory < ApplicationRecord
   
+  #article_category 是文章的标签，鉴于修改model名涉及多处修改，因此暂不修改为article_tag了，后续抽空修改
   validates :name, presence: true
   
   has_many :article_relationships
